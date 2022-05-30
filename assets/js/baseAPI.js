7cca4934aa2a529d56876fd9c4466570b6a7ac4d
lxx0303 <2434921421@qq.com>
2022-05-30 20:34:37 +0800

    完成了主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,19 @@
 $.ajaxPrefilter((option) => {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url = `http://www.liulongbin.top:3007` + option.url;
+    if (option.url.includes('/my/')) {
+        option.headers = {
+            Authorization: localStorage.getItem("token"),
+        };
+    }
+
+    // 每次发送请求回来校验token是否存在，或者是否过期
+    option.complete = res => {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.强制清空token
+            localStorage.removeItem('token');
+            // 2.跳转登陆页面
+            location.href = '/login.html';
+        }
+    }
 });
